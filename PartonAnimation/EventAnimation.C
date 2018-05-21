b07b7a50610bdf24d4e3f567aed8f5866742ca55
Erin Bossard <erinbossard@Erins-MacBook.local>
2018-05-21 15:03:26 -0600

    Beginning animation file

---
+++ PartonAnimation/EventAnimation.C
@@ -0,0 +1,31 @@
+#include "TLatex.h"
+#include "TChain.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TH3.h"
+#include "TMath.h"
+#include "TLorentzVector.h"
+#include "TFile.h"
+#include "TString.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TLegend.h"
+#include "TLine.h"
+#include "TStyle.h"
+#include "TROOT.h"
+#include "TNtuple.h"
+#include "TRegexp.h"
+#include "TProfile.h"
+#include "TArrow.h"
+#include "TEllipse.h"
+#include "TColor.h"
+#include "TPaletteAxis.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
