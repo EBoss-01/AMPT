72336dbc5c60c568e239c9315a01279027a6c1a0
Erin Bossard <erinbossard@Erins-MacBook.local>
2018-05-22 11:12:13 -0600

    Updated code to test filling one vector from the file being read in

--- PartonAnimation/EventAnimation.C
+++ PartonAnimation/EventAnimation.C
@@ -2,8 +2,8 @@
 //This code will be used to read in the file outputs from AMPT and parse them together.
 //This will allow me to reconstruct the parton evolution and make a scattering animation.
 //
-//File 1: parton-initial-afterPropagation.dat
-//File 2: parton-collisionHistory.dat
+//File 1: parton-initial-afterPropagation_1.dat
+//File 2: parton-collisionHistory_1.dat
 //
 //05-21-18
 //------------------------------------------------------------------------------------------
@@ -47,7 +47,7 @@ struct parton {
 // Parton initial momentum
 	float px;
 	float py;
-	float px;
+	float pz;
 
 // Parton initial location
 	float x;
@@ -56,9 +56,29 @@ struct parton {
 	float t;
 
 //Parton mass in GeV
-	float mass;
+	float m;
 
-}
+};
+
+//----------------------------------
+// Variables
+//----------------------------------
+
+// This is the counter to keep track of which event is being read.
+int eventnumber = 0;
+
+// Vector for the scattering times for the specific partons.
+vector<parton> ScatteringTime;
+
+// A vector of other vectors hopefully
+vector<vector<parton> > EventParticles;
+
+// This is my practice vector to be certain that my filling is working as intended.
+vector<parton> PracticeVector;
+
+//----------------------------------
+//Functions
+//----------------------------------
 
 void myText(Double_t x,Double_t y,Color_t color,const char *text,Double_t tsize = 0.05,double angle=-1) {
 	TLatex l;
@@ -67,5 +87,82 @@ void myText(Double_t x,Double_t y,Color_t color,const char *text,Double_t tsize 
 	l.SetTextColor(color);
 	if (angle > 0) l.SetTextAngle(angle);
 	l.DrawLatex(x,y,text);
+}
+
+// This will be my attempt to read in the files that I will be using. 
+void EventAnimation(void) {
+	ifstream myInitialFileInfo;
+	ifstream myEvolutionFile;
+
+	myInitialFileInfo.open("parton-initial-afterPropagation_1.dat");
+
+	if (!myInitialFileInfo) {
+	// This will let me know if the file fails to open and specifically which file.
+		cout << "Unable to open file parton-initial-afterPropagation_1.dat" << endl;
+		return;
+	}
+	else {
+	// I've added this piece simply to confirm that the file did indeed open.
+		cout << "Successfully opened parton-initial-afterPropagation_1.dat" << endl;
+	}
+
+	/*myEvolutionFile.open("parton-collisionHistory_1.dat");
+
+	if (!myEvolutionFile) {
+	// As before this will let me know if this specific file fails to open.
+		cout << "Unable to open file parton-collisionHistory_1.dat" << endl;
+		return;
+	}
+	else {
+	// This is here simply to confirm that this file opened successfully as well.
+		cout << "Successfully opened parton-collisionHistory_1.dat" << endl;
+	}*/
+
+	while (myInitialFileInfo) {
+
+		if (eventnumber % 100 == 0) {
+
+			cout << "Reading Event Number " << eventnumber << endl;
+		}
+
+		// Information needed to read the event header.
+		int iterationN;
+		int nPartons;
+		int nBaryons;
+		int nMesons;
+		int particleC;
+		int particleNC;
+
+		myInitialFileInfo >> eventnumber >> iterationN >> nPartons >> nBaryons >> nMesons >> particleC >> particleNC;
+
+		if (!myInitialFileInfo) break;
+
+		for (int i = 0; i < nPartons; i++) {
+
+			int partID;
+			float momenta[3];
+			float mass;
+			double spacetime[4];
+
+			myInitialFileInfo >> partID >> momenta[0] >> momenta[1] >> momenta[2] >> mass >> spacetime[0] >> spacetime[1] >> spacetime[2] >> spacetime[3];
+
+			cout << partID << ", " << momenta[0] << ", " << momenta[1] << ", " << momenta[2] << ", " << mass << ", " << spacetime[0] << ", " << spacetime[1] << ", " << spacetime[2] << ", " << spacetime[3] << endl;
+
+			parton partinfo;
+			partinfo.px = momenta[0];
+			partinfo.py = momenta[1];
+			partinfo.pz = momenta[2];
+			partinfo.m = mass;
+			partinfo.x = spacetime[0];
+			partinfo.y = spacetime[1];
+			partinfo.z = spacetime[2];
+			partinfo.t = spacetime[3];
+
+			PracticeVector.push_back(partinfo);
+		}
+	}
+
+	myInitialFileInfo.close();
+	return;
 }
 
