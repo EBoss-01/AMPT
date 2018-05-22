03705ad37ca72e30eb30158d77eca23a0ffe34bb
Erin Bossard <erinbossard@Erins-MacBook.local>
2018-05-22 17:07:13 -0600

    Code in progress to include the nested vectors

--- PartonAnimation/EventAnimation.C
+++ PartonAnimation/EventAnimation.C
@@ -71,7 +71,7 @@ int eventnumber = 0;
 vector<parton> ScatteringTime;
 
 // A vector of other vectors hopefully
-vector<vector<parton> > EventParticles;
+vector<vector<parton> > EventPartons;
 
 // This is my practice vector to be certain that my filling is working as intended.
 vector<parton> PracticeVector;
@@ -106,17 +106,17 @@ void EventAnimation(void) {
 		cout << "Successfully opened parton-initial-afterPropagation_1.dat" << endl;
 	}
 
-	/*myEvolutionFile.open("parton-collisionHistory_1.dat");
+	myEvolutionFile.open("parton-collisionsHistory_1.dat");
 
 	if (!myEvolutionFile) {
 	// As before this will let me know if this specific file fails to open.
-		cout << "Unable to open file parton-collisionHistory_1.dat" << endl;
+		cout << "Unable to open file parton-collisionsHistory_1.dat" << endl;
 		return;
 	}
 	else {
 	// This is here simply to confirm that this file opened successfully as well.
-		cout << "Successfully opened parton-collisionHistory_1.dat" << endl;
-	}*/
+		cout << "Successfully opened parton-collisionsHistory_1.dat" << endl;
+	}
 
 	while (myInitialFileInfo) {
 
@@ -135,8 +135,10 @@ void EventAnimation(void) {
 
 		myInitialFileInfo >> eventnumber >> iterationN >> nPartons >> nBaryons >> nMesons >> particleC >> particleNC;
 
+		// This line prevents the file from trying to read the last line multiple times.
 		if (!myInitialFileInfo) break;
 
+		// This loop fills in the initial information for each parton.
 		for (int i = 0; i < nPartons; i++) {
 
 			int partID;
@@ -156,12 +158,88 @@ void EventAnimation(void) {
 			partinfo.z = spacetime[2];
 			partinfo.t = spacetime[3];
 
-			PracticeVector.push_back(partinfo);
+			vector<parton> auxillary;
+			auxillary.push_back(partinfo);
+			EventPartons.push_back(auxillary);
+
+			//PracticeVector.push_back(partinfo);
 		}
 
-		for (int j = 0; j < PracticeVector.size();j++) {
+		/*for (int j = 0; j < PracticeVector.size();j++) {
 			parton p = PracticeVector[j];
 			cout << p.px << endl;
+		}*/
+		//-------------------------------------------
+		// Additional variables for evolution
+		//-------------------------------------------
+		string line;
+		int evt;
+		int junk1;
+		int partonindex1;
+		int partonindex2;
+		// Initial parton information
+		int parton1_id_initial;
+		float parton1_momenta_initial[3];
+		float parton1_mass_initial;
+		double parton1_spacetime_initial[4];
+		int parton2_id_initial;
+		float parton2_momenta_initial[3];
+		float parton2_mass_initial;
+		double parton2_spacetime_initial[4];
+		// Final parton information
+		int parton1_final_id;
+		float parton1_final_momenta[3];
+		float parton1_final_mass;
+		double parton1_final_spacetime[4];
+		int parton2_final_id;
+		float parton2_final_momenta[3];
+		float parton2_final_mass;
+		double parton2_final_spacetime[4];
+
+
+
+		while (std::getline(myEvolutionFile,line)) {
+			stringstream heading(line);
+
+			string description;
+
+			if (heading >> description >> evt >> junk1 >> partonindex1 >> partonindex2) {
+
+				cout << "Event number = " << evt << endl;
+
+				continue;
+			}
+
+			myEvolutionFile >> parton1_id_initial >> parton1_momenta_initial[0] >> parton1_momenta_initial[1] >> parton1_momenta_initial[2] >> parton1_mass_initial >> parton1_spacetime_initial[0] >> parton1_spacetime_initial[1] >> parton1_spacetime_initial[2] >> parton1_spacetime_initial[3];
+
+			myEvolutionFile >> parton2_id_initial >> parton2_momenta_initial[0] >> parton2_momenta_initial[1] >> parton2_momenta_initial[2] >> parton2_mass_initial >> parton2_spacetime_initial[0] >> parton2_spacetime_initial[1] >> parton2_spacetime_initial[2] >> parton2_spacetime_initial[3];
+
+			myEvolutionFile >> parton1_final_id >> parton1_final_momenta[0] >> parton1_final_momenta[1] >> parton1_final_momenta[2] >> parton1_final_mass >> parton1_final_spacetime[0] >> parton1_final_spacetime[1] >> parton1_final_spacetime[2] >> parton1_final_spacetime[3];
+
+			myEvolutionFile >> parton2_final_id >> parton2_final_momenta[0] >> parton2_final_momenta[1] >> parton2_final_momenta[2] >> parton2_final_mass >> parton2_final_spacetime[0] >> parton2_final_spacetime[1] >> parton2_final_spacetime[2] >> parton2_final_spacetime[3];
+
+
+			parton part1;
+			part1.px = parton1_final_momenta[0];
+			part1.py = parton1_final_momenta[1];
+			part1.pz = parton1_final_momenta[2];
+			part1.x = parton1_final_spacetime[0];
+			part1.y = parton1_final_spacetime[1];
+			part1.z = parton1_final_spacetime[2];
+			part1.t = parton1_final_spacetime[3];
+			part1.m = parton1_final_mass;
+
+			parton part2;
+			part2.px = parton2_final_momenta[0];
+			part2.py = parton2_final_momenta[1];
+			part2.pz = parton2_final_momenta[2];
+			part2.x = parton2_final_spacetime[0];
+			part2.y = parton2_final_spacetime[1];
+			part2.z = parton2_final_spacetime[2];
+			part2.t = parton2_final_spacetime[3];
+			part2.m = parton2_final_mass;
+
+			myEvolutionFile.close();
 		}
 	}
 
