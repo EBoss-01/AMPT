1d62a041d7df860056c40883a58e6c6d04b60ea6
Erin Bossard <erinbossard@Erins-MacBook.local>
2018-05-24 15:57:55 -0600

    Most recent update to address jumping issues

--- PartonAnimation/filetest.C
+++ PartonAnimation/filetest.C
@@ -1,55 +1,77 @@
 #include <iostream>
 #include <fstream>
 
-void filetest () {
-
 	// Arrays created to hold the values I need for this test.
 	float xmomentum[4] = {0.162000, 0.111000, -0.090000, -0.073000};
 	float ymomentum[4] = {0.272000, 0.309000, -0.053000, 0.105000};
 	float xposition[4] = {0.18};
 	float yposition[4] = {-0.20};
-	float time[4] = {0.25, 0.27, 0.28, 0.29};
+	float times[4] = {0.25, 0.27, 0.28, 0.29};
 	float partonmass = 0.200;
-	float dt = 0.025; // Timestep in fm/c
-	float Nstep = 100;
-	float actualtime = 0;
+	float dt = 0.0005; // Timestep in fm/c
+	float Nstep = 600;
 	float xvelocity[4];
 	float yvelocity[4];
 
-	for (int i = 0; i < Nstep; i++) {
+// This function is used to determine the stage that will be used for each part of the calculation.
+int getStage(float actualtime) {
 
-		actualtime = dt * i;
+	if (actualtime < times[0]) return -999;
 
-		for (int j = 0; j < 4; j++) {
+	for (int i = 0; i < 4; i++) {
 
-			xvelocity[j] = xmomentum[j]/partonmass;
-			yvelocity[j] = ymomentum[j]/partonmass;
+		if (actualtime > times[i] && actualtime <= times[i+1]) {
 
-			if (j > 0) {
-
-				xposition[j] = xposition[j-1] * time[j];
-				yposition[j] = yposition[j-1] * time[j];
-
-				//cout << "calculated position: " << xposition[j] << endl;
-			}
-
-			if ((j == 3) && (actualtime >= time[3])) {
-				float xt2 = xposition[3] + xvelocity[3] * actualtime;
-				float yt2 = yposition[3] + yvelocity[3] * actualtime;
-
-				cout << "{" << xt2 << "," << yt2 << "}" << "," << endl;
-			}
-
-
-			if ((actualtime < time[j]) && (actualtime >= time[j - 1])) {
-
-				float xt = xposition[j] + xvelocity[j] * actualtime;
-				float yt = yposition[j] + yvelocity[j] * actualtime;
-
-				cout << "{" << xt << "," << yt << "}" << "," << endl;
-			}
-
+			return i;
 		}
 
+		else if (actualtime > times[3]) {
+			return 3;
+		}
+	}
+
+	return 0;
+}
+
+void filetest () {
+
+	for (int i = 0; i < Nstep; i++) {
+
+		float actualtime = i * dt;
+
+		// This is where the stage is determined.
+		int stage = getStage(actualtime);
+
+		if (stage < 0) {
+			continue;
+		}
+
+		// This is to reset the time for when the parton is born. This is also used to reset the time at each scattering.
+		actualtime = actualtime - times[stage];
+
+		// Calculation of the velocity at each stage.
+		xvelocity[stage] = xmomentum[stage]/partonmass;
+		yvelocity[stage] = ymomentum[stage]/partonmass;
+
+		// Determine the initial position of the parton.
+		float x0, y0;
+		if (stage == 0) {
+
+			x0 = xposition[stage];
+			y0 = yposition[stage];
+		}
+		else {
+			x0 = xposition[stage-1] + xvelocity[stage-1] * (times[stage] - times[stage-1]);
+			y0 = yposition[stage-1] + yvelocity[stage-1] * (times[stage] - times[stage-1]);
+
+			xposition[stage] = x0;
+			yposition[stage] = y0;
+		}
+
+		// Calculating the positions as a function of time.
+		float xt = xposition[stage] + (xvelocity[stage] * actualtime);
+		float yt = yposition[stage] + (yvelocity[stage] * actualtime);
+
+		cout << "{" << xt << "," << yt << "}," << endl;
 	}
 }
